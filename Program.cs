88bbf64d938db1059fdbc4645de9c8b892d64c40
ty802 <31832680+ty802@users.noreply.github.com>
2025-12-12 19:58:35 -0600

    fix wanings

--- Program.cs
+++ Program.cs
@@ -44,8 +44,8 @@ public class Program
         procstart = new("git", new string[] { "read-tree", targetname });
         procstart.Environment.Add("GIT_INDEX_FILE", tmp);
         Process.Start(procstart)?.WaitForExit();
-        string[] gargs = new string[6] { "update-index", "--add", "--cacheinfo", null, null, null };
-        string[] rmargs = new string[2] {"rm",null};
+        string[] gargs = new string[6] { "update-index", "--add", "--cacheinfo", string.Empty, string.Empty, string.Empty };
+        string[] rmargs = new string[2] {"rm",string.Empty};
         foreach (Match m in cache)
         {
             if(m.Groups[3].Value == "D")
